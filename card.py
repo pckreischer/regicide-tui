0e001a480e62a67f0c235c8573ceddab74a7bef6
Paul Kreischer <pckreischer5@gmail.com>
2026-08-13 13:06:06 -0500

    selection labels

--- card.py
+++ card.py
@@ -2,6 +2,7 @@ import random
 import curses
 
 CARD_RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+RANK_NAMES = {"A": "Ace", "J": "Jack", "Q": "Queen", "K": "King"}
 CARD_SUITS = {"Spades": "♤", "Hearts": "♡", "Clubs": "♧", "Diamonds": "♢"}
 #CARD_SUITS = {"Spades": "♠", "Hearts": "♥", "Clubs": "♣", "Diamonds": "♦"}
 
@@ -21,7 +22,8 @@ class Card:
         self.suit_icon = CARD_SUITS[suit]
 
     def __str__(self):
-        return f"{self.rank} of {self.suit_name}"
+        rank_name = RANK_NAMES.get(self.rank, self.rank)
+        return f"{rank_name} of {self.suit_name}"
 
     def draw(self, stdscr, top, left):
 
@@ -44,4 +46,7 @@ class Card:
 
         for i, row in enumerate(self.sprite):
             stdscr.addstr(top + i, left, row)
-        
+
+    # method to return a list of strings detailing the card value and effect
+    def generateStats(self):
+        return str(self)
--- main.py
+++ main.py
@@ -16,17 +16,30 @@ def newHand():
         i += 1
 
 def renderHand(stdscr, sh, sw):
-    stdscr.clear()
-    left = (sw // 2) - (MAX_HAND_SIZE // 2 * 6) - 4
-    h = 3 * sh // 4
+    left = (sw // 2) - (MAX_HAND_SIZE // 2 * 6) - 4 # hardcoded card size
+    h = sh - 9
 
     for i in range(len(hand)):
         if i == card_selected:
             hand[i].draw(stdscr, h - 2, left)
+            stdscr.addstr(h - 3, left + 3, "▼") # more hardcoded card size shenanigans but its ok
+            card_stats = hand[i].generateStats()
+            stdscr.addstr(h - 4, (left + 3) - (len(card_stats) // 2), card_stats)
+
+
         else: 
             hand[i].draw(stdscr, h, left)
         left += 7
     stdscr.refresh()
+
+def update(stdscr):
+
+    sh, sw = stdscr.getmaxyx()  # screen height, width
+
+    stdscr.clear()
+    renderHand(stdscr, sh, sw)
+
+
 
 def main(stdscr):
     global card_selected
@@ -37,10 +50,9 @@ def main(stdscr):
     # Let getch() understand arrow keys
     stdscr.keypad(True)
 
-    sh, sw = stdscr.getmaxyx()  # screen height, width
 
     newHand()
-    renderHand(stdscr, sh, sw)
+    update(stdscr)
 
     while True:
         key = stdscr.getkey()
@@ -51,7 +63,7 @@ def main(stdscr):
             card_selected = min(len(hand) - 1, card_selected + 1)
         elif key == "q":
             break
-        renderHand(stdscr, sh, sw)
+        update(stdscr)
 
 
 curses.wrapper(main)
