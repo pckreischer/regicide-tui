0ff5abb3e3b32738109decee9fd9fb4f0125a2e7
Paul Kreischer <pckreischer5@gmail.com>
2026-08-13 11:32:46 -0500

    you can select a card now (awesome)

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 import curses
 import card
 
+MAX_HAND_SIZE = 8
+
 deck = card.generate_deck()
-selected = 0 
 
 hand = []
+card_selected = 0
 
 def newHand():
     hand.clear()
     i = 0
-    while i < 8: # hardcoded hand size
+    while i < MAX_HAND_SIZE:
         hand.append(deck.pop())
         i += 1
 
 def renderHand(stdscr, sh, sw):
-    left = (sw // 2) - (4 * 6) - 3 # more hardcoded hand size
-    for card_obj in hand:
-        card_obj.draw(stdscr, sh // 2, left)
-        left += 8
+    stdscr.clear()
+    left = (sw // 2) - (MAX_HAND_SIZE // 2 * 6) - 4
+    h = 3 * sh // 4
+
+    for i in range(len(hand)):
+        if i == card_selected:
+            hand[i].draw(stdscr, h - 2, left)
+        else: 
+            hand[i].draw(stdscr, h, left)
+        left += 7
     stdscr.refresh()
 
 def main(stdscr):
+    global card_selected
     # Hide the blinking cursor
     curses.curs_set(0)
     # Don't wait for Enter — read keys immediately
@@ -33,7 +42,16 @@ def main(stdscr):
     newHand()
     renderHand(stdscr, sh, sw)
 
-    stdscr.getkey()
+    while True:
+        key = stdscr.getkey()
+
+        if key == "KEY_LEFT":
+            card_selected = max(0, card_selected - 1)
+        elif key == "KEY_RIGHT":
+            card_selected = min(len(hand) - 1, card_selected + 1)
+        elif key == "q":
+            break
+        renderHand(stdscr, sh, sw)
 
 
 curses.wrapper(main)
